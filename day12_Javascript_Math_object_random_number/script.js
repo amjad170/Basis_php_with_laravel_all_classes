56fd8d22096b7aa3dd6aab553f277bdc11d08fc6
Simanta pau <amjadhossain17416762@gmail.com>
2023-04-30 10:22:35 +0600

    add classes

---
+++ day12_Javascript_Math_object_random_number/script.js
@@ -0,0 +1,32 @@
+function genBudget(min,max){
+    var output = document.getElementById('output');
+
+    var setup =max-min+1
+    var setup1 = Math.random()*setup;
+    var result = Math.floor(setup1)+min;
+
+    output.innerHTML = result;
+}
+
+
+////Math object practice
+/* let val;
+val = Math.PI;
+val = Math.E;
+val = Math.floor(2.99);
+val = Math.ceil(2.33);
+val = Math.round(2.5); //if floot point is >=5 round  will 3 other wise 2
+val = Math.min(20,67,87,34); 
+val = Math.max(20,67,87,34); 
+val = Math.pow(2,3); 
+val = Math.sqrt(36); 
+val = Math.random()*10+1; 
+
+
+console.log(val) */
+
+if(true){
+    let a =20
+}
+
+console.log(a)
