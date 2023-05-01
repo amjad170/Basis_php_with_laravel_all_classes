e596b680016cc77dc796e3f5dd92c65706598459
Simanta pau <amjadhossain17416762@gmail.com>
2023-05-02 01:28:36 +0600

    ok

---
+++ day08_day9_javaScript/script.js
@@ -0,0 +1,381 @@
+function result1(){
+    let tMark=document.querySelector(".tMark").value;
+    let pMark=document.querySelector(".pMark").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade").value="A+";
+        document.querySelector(".point").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade").value="A";
+        document.querySelector(".point").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade").value="A-";
+        document.querySelector(".point").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade").value="B";
+        document.querySelector(".point").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade").value="c";
+        document.querySelector(".point").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade").value="D";
+        document.querySelector(".point").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade").value="F";
+        document.querySelector(".point").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+
+function result2(){
+    let tMark=document.querySelector(".tMark2").value;
+    let pMark=document.querySelector(".pMark2").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark2").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade2").value="A+";
+        document.querySelector(".point2").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade2").value="A";
+        document.querySelector(".point2").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade2").value="A-";
+        document.querySelector(".point2").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade2").value="B";
+        document.querySelector(".point2").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade2").value="c";
+        document.querySelector(".point2").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade2").value="D";
+        document.querySelector(".point2").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade2").value="F";
+        document.querySelector(".point2").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+function result3(){
+    let tMark=document.querySelector(".tMark3").value;
+    let pMark=document.querySelector(".pMark3").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark3").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade3").value="A+";
+        document.querySelector(".point3").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade3").value="A";
+        document.querySelector(".point3").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade3").value="A-";
+        document.querySelector(".point3").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade3").value="B";
+        document.querySelector(".point3").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade3").value="c";
+        document.querySelector(".point3").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade3").value="D";
+        document.querySelector(".point3").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade3").value="F";
+        document.querySelector(".point3").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+function result4(){
+let tMark=document.querySelector(".tMark4").value;
+    let pMark=document.querySelector(".pMark4").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark4").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade4").value="A+";
+        document.querySelector(".point4").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade4").value="A";
+        document.querySelector(".point4").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade4").value="A-";
+        document.querySelector(".point4").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade4").value="B";
+        document.querySelector(".point4").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade4").value="c";
+        document.querySelector(".point4").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade4").value="D";
+        document.querySelector(".point4").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade4").value="F";
+        document.querySelector(".point4").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+function result5(){
+    let tMark=document.querySelector(".tMark5").value;
+    let pMark=document.querySelector(".pMark5").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark5").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade5").value="A+";
+        document.querySelector(".point5").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade5").value="A";
+        document.querySelector(".point5").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade5").value="A-";
+        document.querySelector(".point5").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade5").value="B";
+        document.querySelector(".point5").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade5").value="c";
+        document.querySelector(".point5").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade5").value="D";
+        document.querySelector(".point5").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade5").value="F";
+        document.querySelector(".point5").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+function result6(){
+    let tMark=document.querySelector(".tMark6").value;
+    let pMark=document.querySelector(".pMark6").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark6").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade6").value="A+";
+        document.querySelector(".point6").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade6").value="A";
+        document.querySelector(".point6").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade6").value="A-";
+        document.querySelector(".point6").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade6").value="B";
+        document.querySelector(".point6").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade6").value="c";
+        document.querySelector(".point6").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade6").value="D";
+        document.querySelector(".point6").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade6").value="F";
+        document.querySelector(".point6").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+function result7(){
+    let tMark=document.querySelector(".tMark7").value;
+    let pMark=document.querySelector(".pMark7").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark7").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade7").value="A+";
+        document.querySelector(".point7").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade7").value="A";
+        document.querySelector(".point7").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade7").value="A-";
+        document.querySelector(".point7").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade7").value="B";
+        document.querySelector(".point7").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade7").value="c";
+        document.querySelector(".point7").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade7").value="D";
+        document.querySelector(".point7").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade7").value="F";
+        document.querySelector(".point7").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+function result8(){
+    let tMark=document.querySelector(".tMark8").value;
+    let pMark=document.querySelector(".pMark8").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark8").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade8").value="A+";
+        document.querySelector(".point8").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade8").value="A";
+        document.querySelector(".point8").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade8").value="A-";
+        document.querySelector(".point8").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade8").value="B";
+        document.querySelector(".point8").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade8").value="c";
+        document.querySelector(".point8").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade8").value="D";
+        document.querySelector(".point8").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade8").value="F";
+        document.querySelector(".point8").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+function result9(){
+    let tMark=document.querySelector(".tMark9").value;
+    let pMark=document.querySelector(".pMark9").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark9").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade9").value="A+";
+        document.querySelector(".point9").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade9").value="A";
+        document.querySelector(".point9").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade9").value="A-";
+        document.querySelector(".point9").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade9").value="B";
+        document.querySelector(".point9").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade9").value="c";
+        document.querySelector(".point9").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade9").value="D";
+        document.querySelector(".point9").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade9").value="F";
+        document.querySelector(".point9").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
+function result10(){
+    let tMark=document.querySelector(".tMark10").value;
+    let pMark=document.querySelector(".pMark10").value;
+    let total=Number(tMark) + Number(pMark);
+    document.querySelector(".totalMark10").value=total;
+
+    if(total>=80 && total<=100){
+        document.querySelector(".grade10").value="A+";
+        document.querySelector(".point10").value="5.0";
+    }
+    else if(total>=70 && total<=79){
+        document.querySelector(".grade10").value="A";
+        document.querySelector(".point10").value="4.0";
+    }
+    else if(total>=60 && total<=69){
+        document.querySelector(".grade10").value="A-";
+        document.querySelector(".point10").value="3.5";
+    }
+    else if(total>=50 && total<=59){
+        document.querySelector(".grade10").value="B";
+        document.querySelector(".point10").value="3.0";
+    }
+    else if(total>=40 && total<=40){
+        document.querySelector(".grade10").value="c";
+        document.querySelector(".point10").value="2.0";
+    }
+    else if(total>=33 && total<=39){
+        document.querySelector(".grade10").value="D";
+        document.querySelector(".point10").value="1.0";
+    }
+    else if(total>=0 && total<=32){
+        document.querySelector(".grade10").value="F";
+        document.querySelector(".point10").value="0.0";
+    }
+    else{
+      alert("Invalid Input")
+    }
+}
